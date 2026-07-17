925b741f7d622c8425bf4aff3c03ef0db98c2296
Hosea Javier <hosea.j16@gmail.com>
2026-07-17 15:14:09 +0700

    Community Connect Directory Website

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
-import { Footer } from './components/Footer';
-import { Home } from './pages/Home';
-import { Directory } from './pages/Directory';
-import { ServiceDetail } from './pages/ServiceDetail';
-import { Contact } from './pages/Contact';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
+import { Directory } from "./pages/Directory";
+import { ServiceDetail } from "./pages/ServiceDetail";
+import { Contact } from "./pages/Contact";
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-[#F8FAFC] text-[#1E293B] antialiased flex flex-col font-sans">
-        <a 
-          href="#main-content" 
-          className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 bg-blue-600 text-white px-4 py-2 rounded-lg font-bold z-[100] shadow-md"
-        >
-          Skip to main content
-        </a>
-
         <Navbar />
 
         <main id="main-content" className="flex-grow">
